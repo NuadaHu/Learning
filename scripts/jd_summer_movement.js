b301590d42b9ba2775c6587a7afe64c2e660b85f
18511520939 <272843613@qq.com>
2021-07-22 14:56:51 +0800

    update

--- scripts/jd_summer_movement.js
+++ scripts/jd_summer_movement.js
@@ -36,49 +36,120 @@ let smashUtils;
 //是否兑换红包
 let withdraw = false;
 const UA =  `jdpingou;iPhone;10.0.6;${Math.ceil(Math.random()*2+12)}.${Math.ceil(Math.random()*4)};${randomString(40)};`;
-class MovementFaker {
-  constructor(cookie) {this.cookie = cookie;this.ua = UA;}
-  async run() {if (!smashUtils) {await this.init();}
+class MoveMentFaker {
+  constructor(cookie) {
+    // this.secretp = secretp;
+    this.cookie = cookie;
+  }
+
+  async run() {
+    if (!smashUtils) {
+      await this.init();
+    }
+
     var t = Math.floor(1e7 + 9e7 * Math.random()).toString();
-    var e = smashUtils.get_risk_result({id: t,data: {random: t}}).log;
-    var o = JSON.stringify({extraData: {log:  e || -1,sceneid: DATA.sceneid,},random: t});
+    var e = smashUtils.get_risk_result({
+      id: t,
+      data: {
+        random: t
+      }
+    }).log;
+    var o = JSON.stringify({
+      extraData: {
+        log: e || -1,
+          // log: encodeURIComponent(e),
+          sceneid: DATA.sceneid,
+      },
+      // secretp: this.secretp,
+      random: t
+    })
+
+    // console.log(o);
     return o;
   }
+
   async init() {
     try {
-      console.time('MovementFaker');process.chdir(__dirname);const html = await MovementFaker.httpGet(URL);const script = REG_SCRIPT.exec(html);
-      if (script) {const [, scriptUrl, filename] = script;const jsContent = await this.getJSContent(filename, scriptUrl);const fnMock = new Function;const ctx = {window: { addEventListener: fnMock },document: {addEventListener: fnMock,removeEventListener: fnMock,cookie: this.cookie,},navigator: { userAgent: this.ua },};vm.createContext(ctx);vm.runInContext(jsContent, ctx);smashUtils = ctx.window.smashUtils;smashUtils.init(DATA);
+      // console.time('MoveMentFaker');
+      process.chdir(__dirname);
+      const html = await MoveMentFaker.httpGet(URL);
+      const script = REG_SCRIPT.exec(html);
+
+      if (script) {
+        const [, scriptUrl, filename] = script;
+        const jsContent = await this.getJSContent(filename, scriptUrl);
+        const fnMock = new Function;
+        const ctx = {
+          window: { addEventListener: fnMock },
+          document: {
+            addEventListener: fnMock,
+            removeEventListener: fnMock,
+            cookie: this.cookie
+          },
+          navigator: { userAgent: UA }
+        };
+
+        vm.createContext(ctx);
+        vm.runInContext(jsContent, ctx);
+        smashUtils = ctx.window.smashUtils;
+        smashUtils.init(DATA);
+
+        // console.log(ctx);
       }
-      console.timeEnd('MovementFaker');
+
+      // console.log(html);
+      // console.log(script[1],script[2]);
+      // console.timeEnd('MoveMentFaker');
     } catch (e) {
       console.log(e)
     }
   }
+
   async getJSContent(cacheKey, url) {
-    try {await fs.access(cacheKey, R_OK);const rawFile = await fs.readFile(cacheKey, { encoding: 'utf8' });return rawFile;
+    try {
+      await fs.access(cacheKey, R_OK);
+      const rawFile = await fs.readFile(cacheKey, { encoding: 'utf8' });
+
+      return rawFile;
     } catch (e) {
-      let jsContent = await MovementFaker.httpGet(url);
+      let jsContent = await MoveMentFaker.httpGet(url);
       const moduleIndex = jsContent.indexOf(SYNTAX_MODULE, 1);
       const findEntry = REG_ENTRY.test(jsContent);
       if (!(moduleIndex && findEntry)) {
         throw new Error('Module not found.');
       }
+        // const needModuleId = jsContent.substring(moduleIndex-20, moduleIndex).match(/(\d+):function/)[1]
       jsContent = jsContent.replace(REG_ENTRY, `$1${needModuleId}`);
       fs.writeFile(cacheKey, jsContent);
       return jsContent;
+
       REG_ENTRY.lastIndex = 0;
       const entry = REG_ENTRY.exec(jsContent);
+
+      console.log(moduleIndex, needModuleId);
+      console.log(entry[1], entry[2]);
     }
   }
+
   static httpGet(url) {
     return new Promise((resolve, reject) => {
       const protocol = url.indexOf('http') !== 0 ? 'https:' : '';
-      const req = https.get(protocol + url, (res) => {res.setEncoding('utf-8');let rawData = '';res.on('error', reject);res.on('data', chunk => rawData += chunk);res.on('end', () => resolve(rawData));});
+      const req = https.get(protocol + url, (res) => {
+        res.setEncoding('utf-8');
+
+        let rawData = '';
+
+        res.on('error', reject);
+        res.on('data', chunk => rawData += chunk);
+        res.on('end', () => resolve(rawData));
+      });
+
       req.on('error', reject);
       req.end();
     });
   }
 }
+
 $.appid = 'o2_act';
 $.inviteList = [];
 $.byInviteList = [];
@@ -259,7 +330,13 @@ async function main(){
   }
 }
 
-async function getBody($) {const zf = new MovementFaker($.cookie);const ss = await zf.run();return ss;}
+async function getBody($) {
+  const zf = new MoveMentFaker($.cookie);
+  // const zf = new MoveMentFaker($.secretp, $.cookie);
+  const ss = await zf.run();
+
+  return ss;
+}
 
 async function doTask(){
   //做任务
