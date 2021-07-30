18c05371509545210aeda609c4ab5389e9d348c3
18511520939 <272843613@qq.com>
2021-07-30 08:55:21 +0800

    update

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -39,9 +39,9 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 let tuanActiveId = ``, hasSend = false;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
-let cookiesArr = ['T_zZaWP6by9yA1wehxM4mg=='], cookie = '', message = '', allMessage = '';
+let cookiesArr = [], cookie = '', message = '', allMessage = '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-$.tuanIds = ['ih298pJ3XMpVNlK9t32Duw=='];
+$.tuanIds = [];
 $.appId = 10001;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
