acda02afb3a1c76ffb0f0bd3a56f1319fdc92cb3
18511520939 <272843613@qq.com>
2021-08-05 01:12:32 +0800

    update

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+import os
 import json
+import qrcode
 import time
+import pyperclip
 import requests
-import logging
+import threading
 requests.packages.urllib3.disable_warnings()
-from urllib.parse import urlencode, quote_plus
+os.environ['no_proxy'] = '*'
 
-
-logging.basicConfig(level=logging.INFO, format='%(message)s')
-logger = logging.getLogger(__name__)
-
-jd_ua = 'jdapp;android;10.0.5;11;{0};network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36'
+jd_ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.14.0 main/1.0 baiduboxapp/11.18.0.16 (Baidu; P2 13.3.1) NABar/0.0 TM/{0}'
 
 
 def token_get():
@@ -48,20 +47,16 @@ def token_post(s_token):
     c = s.cookies.get_dict()
     okl_token = c['okl_token']
     # print("okl_token:", okl_token)
-    qrurl = 'https://plogin.m.jd.com/cgi-bin/m/tmauth?client_type=m&appid=300&token={0}'.format(token)
-    logger.info("请手动复制链接到在线二维码生成网站（cli.im）,并扫码登录")
-    logger.info('')
-    logger.info(qrurl)
-    logger.info('')
-    logger.info('或者访问以下链接直接生成2维码图片扫码')
-    payload = {'data': qrurl}
-    result = urlencode(payload, quote_via=quote_plus)
-    logger.info('https://api.qrserver.com/v1/create-qr-code/?size=300x300&{0}'.format(result))
-
-    check_token(token, okl_token)
+    t1 = threading.Thread(target=image_print, args=(token,))
+    t2 = threading.Thread(target=check_token, args=(token, okl_token))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
 
 
 def check_token(token, okl_token):
+    global i
     t = round(time.time() * 1000)
     headers = {
         'User-Agent': jd_ua.format(t),
@@ -79,30 +74,52 @@ def check_token(token, okl_token):
     check = json.loads(res.text)
     code = check['errcode']
     message = check['message']
-    global i
     while code == 0:
-        logger.info("扫码成功")
+        print("扫码成功")
         jd_ck = s.cookies.get_dict()
         pt_key = 'pt_key=' + jd_ck['pt_key']
         pt_pin = 'pt_pin=' + jd_ck['pt_pin']
         ck = str(pt_key) + ';' + str(pt_pin) + ';'
-        logger.info(ck)
+        print(ck)
+        pyperclip.copy(ck)
+        print("已复制到剪切板")
         break
     else:
         i = i + 1
-        if i < 60:
-            logger.info(message)
-            time.sleep(3)
+        # print(i)
+        if i < 120:
+            print(message)
+            time.sleep(1)
             check_token(token, okl_token)
         else:
             exit(0)
 
 
+def image_print(token):
+    qrurl = 'https://plogin.m.jd.com/cgi-bin/m/tmauth?client_type=m&appid=300&token={0}'.format(token)
+    print("如果系统未弹出二维码图片, 请手动生成二维码")
+    print("为防止死循环 加入60次未获取到cookie后退出")
+    print("URL地址已复制到剪切板, 请使用在线二维码生成")
+    print()
+    print(qrurl)
+    print()
+    pyperclip.copy(qrurl)
+    try:
+        img = qrcode.make(qrurl)
+        img.show()
+    except:
+        print('二维码生成失败, 请手动在线生成')
+        print(qrurl)
+
+
 if __name__ == '__main__':
-    logger.info("Ver: 1.0.4 By: limoe 面板专用版本")
-    logger.info("https://github.com/Zy143L/jd_cookie")
-    logger.info("JD扫码获取Cookie")
+    os.system('chcp 65001')
+    print("Ver: 1.2 By: limoe")
+    print("https://github.com/Zy143L/jd_cookie")
+    print("JD扫码获取Cookie")
+    print("回车生成二维码")
+    input()
     i = 1
     s = requests.session()
     token_get()
-    exit(0)
+    os.system('pause')
