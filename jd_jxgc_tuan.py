0d21189cb98edabb9086de4b1d0e776d5a873106
18511520939 <272843613@qq.com>
2021-08-11 16:15:25 +0800

    update

--- jd_jxgc_tuan.py
+++ jd_jxgc_tuan.py
@@ -513,6 +513,7 @@ def CreateTuan(ck):
         print("CreateTuan Error", e)
 
 def JoinTuan(ck, tuanId, u, suser, user):
+    tuanId = 'Erz7qsDh-2J4UDmo3jqbHQ=='
     global cookiesList
     try:
         _time = stimestamp()
