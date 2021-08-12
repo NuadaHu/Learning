0911316ba3864cdda5d65afdce4e47230522f372
18511520939 <272843613@qq.com>
2021-08-12 09:57:13 +0800

    update

--- jd_fan.js
+++ jd_fan.js
@@ -20,6 +20,7 @@ const activityList = [
     {'actid':'a422e7bb8cee4092b3b5bc91a0d8fa61','endTime':1629129600000},
     {'actid':'8afc9104d6444696b3f16ceb23a24536','endTime':1630425599000},
     {'actid':'f006443799d34b55b9061be7b765c3fa','endTime':1630339200000},
+    {'actid':'5cd5b2ad1d284ea6bcc4b7e05baf4b7d','endTime':1628611200000},//需要入会
 ];
 if ($.isNode()) {
     Object.keys(jdCookieNode).forEach((item) => {
