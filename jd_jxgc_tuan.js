5f06c34f61616c56bb1a4af7cb7813acec955e4a
18511520939 <272843613@qq.com>
2021-08-17 11:11:14 +0800

    update

--- jd_jxgc_tuan.js
+++ jd_jxgc_tuan.js
@@ -20,7 +20,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const openTuanCK = $.isNode() ? (process.env.OPEN_DREAMFACTORY_TUAN ? process.env.OPEN_DREAMFACTORY_TUAN : '1'):'1';
 let tuanActiveId = ``;
 let cookiesArr = [], cookie = '', message = '';
-$.tuanIds = ['JvOav8q5N7Z8-au2GeVNqg=='];
+$.tuanIds = ['yOe665FeARUrqrW1hbt6eg=='];
 $.appId = 10001;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
@@ -96,7 +96,7 @@ if ($.isNode()) {
       if ((cookiesArr && cookiesArr.length >= ($.tuanNum || 5)) && $.canHelp) {
         for (let j = 0; j < $.tuanIds.length; j++) {
           let item = $.tuanIds[j];
-          if ($.index == 1) {item = 'JvOav8q5N7Z8-au2GeVNqg=='}
+          if ($.index == 1) {item = 'yOe665FeARUrqrW1hbt6eg=='}
           $.tuanMax = false;
           if (!$.canHelp) break;
           console.log(`账号${$.UserName} 去参加团 ${item}`);
